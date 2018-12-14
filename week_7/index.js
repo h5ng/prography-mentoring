015d145ca928a05f32bad5e3941f785d3b374ff8
Q00 <jqyu.lee@gmail.com>
2018-12-14 22:57:14 +0900

    add jwt verify function

--- week_7/index.js
+++ week_7/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
- 
+const cookieParser = require("cookie-parser") 
 const jwt = require("jsonwebtoken");
 const secret_key = require("./config/jwt.json")
 
 const app = express()
+app.use(cookieParser())
 app.get("/login/:id/:pw", function(req,res,next){
 
     // default : HMAC SHA256
@@ -24,6 +25,27 @@ app.get("/login/:id/:pw", function(req,res,next){
 	})
     }
 })
+
+app.get("/verify", 
+    function(req,res,next){
+	let token = req.cookies.user
+	let decode_token = jwt.verify(token, secret_key['secret_key'])
+	if( decode_token.email == 'foo@bar.com'){
+	    next()
+	}else{
+	    res.sendStatus(403)
+	}
+
+    },
+    function(req,res,next){
+    let token = req.cookies.user
+    let decode_token = jwt.verify(token, secret_key['secret_key'])
+    res.json({
+	decode_token: decode_token
+    })
+    
+})
+   
 console.log(secret_key['secret_key'])
 app.listen(3000)
 
